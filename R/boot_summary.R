b9a75d6e82717a31ba3818ef7bbbdea806ff16dc
Måns Thulin <mans@statistikkonsult.com>
2021-06-01 07:10:02 +0000

    version 0.1.0

---
+++ R/boot_summary.R
@@ -0,0 +1,109 @@
+#' Summarising Regression Models Using the Bootstrap
+#'
+#' Summaries for regression models, including "lm", "glm", "nls", and "merMod" ("lmer", "glmer") objects, using the bootstrap for p-values and confidence intervals.
+#'
+#' @param model An object fitted using "lm", "glm", "nls", "lmer" or "glmer".
+#' @param type A vector of character strings representing the type of interval to base the test on. The value should be one of "norm", "basic", "stud", "perc" (the default), and "bca". "stud" and "bca" are not available for "lmer" and "glmer" models.
+#' @param method The method used for bootstrapping. For "lm", "glm", and "nls" objects either "residual" (for resampling of scaled and centred residuals, the default) or "case" (for case resampling). For "merMod" objects either "parametric" (the default) or "semiparametric".
+#' @param conf.level The confidence level for the confidence intervals. The default is 0.95.
+#' @param R The number of bootstrap replicates. The default is 999.
+#' @param pval_precision The desired precision for the p-value. The default is 1/R.
+#' @param adjust.method Adjustment of p-values for multiple comparisons using \code{p.adjust}. The default is "none", in which case the p-values aren't adjusted. The other options are "holm", "hochberg", "hommel", "bonferroni", "BH", "BY", and "fdr"; see \code{?p.adjust} for details on these methods.
+#' @param ... Additional arguments passed to \code{Boot} or \code{bootMer}, such as \code{parallel} for parallel computations. See \code{?car::Boot} and \code{?lme4::bootMer} for details.
+#'
+#' @return A data frame containing coefficient estimates, bootstrap confidence intervals, and bootstrap p-values.
+#' @details p-values can be computed by inverting the corresponding confidence intervals, as described in Section 12.2 of Thulin (2021) and Section 3.12 in Hall (1992). This function computes p-values in this way from "boot" objects. The approach relies on the fact that:
+#' - the p-value of the two-sided test for the parameter theta is the smallest alpha such that theta is not contained in the corresponding 1-alpha confidence interval,
+#' - for a test of the parameter theta with significance level alpha, the set of values of theta that aren't rejected by the two-sided test (when used as the null hypothesis) is a 1-alpha confidence interval for theta.
+#'
+#' The function can be used with "lm", "glm", "nls", and "merMod" ("lmer", "glmer") objects. In addition, it should work for any regression model such that: \code{residuals(object, type="pearson")} returns Pearson residuals; \code{fitted(object)} returns fitted values; \code{hatvalues(object)} returns the leverages, or perhaps the value 1 which will effectively ignore setting the hatvalues. In addition, the \code{data} argument should contain no missing values among the columns actually used in fitting the model.
+#' @importFrom Rdpack reprompt
+#' @references
+#'  \insertRef{hall92}{boot.pval}
+#'
+#'  \insertRef{thulin21}{boot.pval}
+#' @examples
+#' # Bootstrap summary of a linear model for mtcars:
+#' model <- lm(mpg ~ hp + vs, data = mtcars)
+#' boot_summary(model, R = 99)
+#' # (Values for R greater than 99 are recommended for most applications.)
+#'
+#' # Adjust p-values for multiplicity using Holm's method:
+#' boot_summary(model, R = 99, adjust.method = "holm")
+#' @export
+boot_summary <- function(model,
+                      type = "perc",
+                      method = NULL,
+                      conf.level = 0.95,
+                      R = 999,
+                      pval_precision = NULL,
+                      adjust.method = "none",
+                      ...)
+{
+  # Bootstrap the regression model:
+  # (Different functions are used depending on the type of model.)
+  if(class(model) %in% c("lmerMod", "glmerMod"))
+  {
+    # Use lme4::bootMer for mixed models:
+    if(is.null(method)) { method <- "parametric" }
+    boot_res <- lme4::bootMer(model,
+                              FUN = lme4::fixef,
+                              type = method,
+                              nsim = R,
+                              ...)
+  } else {
+    # Use car::Boot for other objects, including lm, glm and nls objects:
+    if(is.null(method)) { method <- "residual" }
+    # For Boot to work inside this function, we must export the car environment
+    # to the global environment
+    # (see https://cran.r-project.org/web/packages/car/vignettes/embedding.pdf).
+    assign(".carEnv", car::.carEnv, envir = parent.frame())
+    boot_res <- car::Boot(model,
+                          method = method,
+                          R = R,
+                          ...)
+  }
+
+  # Create data frame to store results in:
+  if(class(model) %in% c("lmerMod", "glmerMod")) {
+    estimates <- lme4::fixef(model)
+    } else estimates <- model$coefficients
+  p <- length(estimates)
+  results <- data.frame(Estimate = estimates,
+                       `Lower bound` = rep(NA, p),
+                       `Upper bound` = rep(NA, p),
+                       `p-value` = rep(NA, p))
+
+  # Compute confidence intervals:
+  for(i in 1:p)
+  {
+      ci <- boot::boot.ci(boot_res, conf = conf.level, type = type, index = i, ...)
+      results[i, 2:3] <- switch(type,
+                       norm = ci$normal[,2:3],
+                       basic = ci$basic[,4:5],
+                       stud = ci$student[,4:5],
+                       perc = ci$percent[,4:5],
+                       bca = ci$bca[,4:5])
+  }
+
+  # Compute p-values:
+  for(i in 1:p)
+  {
+    results[i, 4] <- boot.pval(boot_res,
+                              type = type,
+                              theta_null = 0,
+                              pval_precision = pval_precision,
+                              index = i,
+                              ...)
+  }
+  if(adjust.method != "none") {
+    results$`Adjusted p-value` <- round(stats::p.adjust(results[, 4], method = adjust.method), round(log10(R)))
+  }
+  # Round p-values:
+  results[,4] <- round(results[,4], round(log10(R)))
+
+
+  return(results)
+}
+
+
---
+++ README.md
@@ -0,0 +1,97 @@
+# boot.pval
+This R package provides functions for computing bootstrap p-values based on `boot` objects, and convenience functions for bootstrap confidence intervals and p-values for various regression models.
+
+## Installation
+To install the package from Github:
+
+```
+library(devtools)
+install_github("mthulin/boot.pval")
+```
+
+## Background
+p-values can be computed by inverting the corresponding confidence intervals, as described in Section 12.2 of [Thulin (2021)](http://www.modernstatisticswithr.com/mathschap.html#confintequal) and Section 3.12 in [Hall (1992)](https://www.springer.com/gp/book/9780387977201). This package contains functions for computing bootstrap p-values in this way from `boot` objects. The approach relies on the fact that:
+
+- The p-value of the two-sided test for the parameter theta is the smallest alpha such that theta is not contained in the corresponding 1-alpha confidence interval,
+- For a test of the parameter theta with significance level alpha, the set of values of theta that aren't rejected by the two-sided test (when used as the null hypothesis) is a 1-alpha confidence interval for theta.
+
+
+## Summaries for regression models
+Summary tables with confidence intervals and p-values for the coefficients of regression models can be obtained using the `boot_summary` function. Currently, the following models are supported:
+
+- Linear models fitted using `lm`,
+- Generalised linear models fitted using `glm` or `glm.nb`,
+- Nonlinear models fitted using `nls`,
+- Linear mixed models fitted using `lme4::lmer`,
+- Generalised linear mixed models fitted using `lme4::glmer`.
+- Cox PH regression models fitted using `survival::coxph` (using `censboot_summary`).
+- Accelerated failure time models fitted using `survival::survreg` (using `censboot_summary`).
+- Any regression model such that: `residuals(object, type="pearson")` returns Pearson residuals; `fitted(object)` returns fitted values; `hatvalues(object)` returns the leverages, or perhaps the value 1 which will effectively ignore setting the hatvalues. In addition, the `data` argument should contain no missing values among the columns actually used in fitting the model.
+
+Here is an example with a linear regression model for the `mtcars` data:
+
+```
+# Bootstrap summary of a linear model for mtcars:
+model <- lm(mpg ~ hp + vs, data = mtcars)
+boot_summary(model)
+
+# Use 9999 bootstrap replicates and adjust p-values for
+# multiplicity using Holm's method:
+boot_summary(model, R = 9999, adjust.method = "holm")
+```
+
+And a toy example for a generalised linear mixed model (using a small number of bootstrap repetitions):
+
+```
+library(lme4)
+model <- glmer(TICKS ~ YEAR + (1|LOCATION),
+           data = grouseticks, family = poisson)
+boot_summary(model, R = 99)
+```
+
+Survival regression models should be fitted using the argument `model = TRUE`. A summary table can then be obtained using `censboot_summary`. By default, the table contains exponentiated coefficients (i.e. hazard ratios, in the case of a Cox PH model).
+
+```
+library(survival)
+# Weibull AFT model:
+model <- survreg(formula = Surv(time, status) ~ age + sex, data = lung,
+                dist = "weibull", model = TRUE)
+censboot_summary(model)
+
+# Cox PH model:
+model <- coxph(formula = Surv(time, status) ~ age + sex, data = lung,
+               model = TRUE)
+# Table with hazard ratios:
+censboot_summary(model)
+# Table with original coefficients:
+censboot_summary(model, coef = "raw")
+```
+
+## Other hypothesis tests
+Bootstrap p-values for hypothesis tests can be obtained using the `boot.pval` function. The following examples are extensions of those given in the documentation for `boot::boot`:
+
+```
+# Hypothesis test for the city data
+# H0: ratio = 1.4
+library(boot)
+ratio <- function(d, w) sum(d$x * w)/sum(d$u * w)
+city.boot <- boot(city, ratio, R = 999, stype = "w", sim = "ordinary")
+boot.pval(city.boot, theta_null = 1.4)
+
+# Studentized test for the two sample difference of means problem
+# using the final two series of the gravity data.
+diff.means <- function(d, f)
+{
+  n <- nrow(d)
+  gp1 <- 1:table(as.numeric(d$series))[1]
+  m1 <- sum(d[gp1,1] * f[gp1])/sum(f[gp1])
+  m2 <- sum(d[-gp1,1] * f[-gp1])/sum(f[-gp1])
+  ss1 <- sum(d[gp1,1]^2 * f[gp1]) - (m1 *  m1 * sum(f[gp1]))
+  ss2 <- sum(d[-gp1,1]^2 * f[-gp1]) - (m2 *  m2 * sum(f[-gp1]))
+  c(m1 - m2, (ss1 + ss2)/(sum(f) - 2))
+}
+grav1 <- gravity[as.numeric(gravity[,2]) >= 7, ]
+grav1.boot <- boot(grav1, diff.means, R = 999, stype = "f",
+                   strata = grav1[ ,2])
+boot.pval(grav1.boot, type = "stud", theta_null = 0)
+```
